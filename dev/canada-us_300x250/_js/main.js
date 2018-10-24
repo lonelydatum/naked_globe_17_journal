f35f05bd3613b433a430a0f9bb2148ecf898a1aa
Gar <gar@lonelydatu.com>
2018-10-24 17:23:51 -0400

    one

--- dev/canada-us_300x250/_js/main.js
+++ dev/canada-us_300x250/_js/main.js
@@ -45,8 +45,8 @@ function start() {
 	tl.to("#bg", 2, {x:"+=50", y:"-=50", opacity:0}, "+=1")
 	
 	tl.from(["#t0"], .5, {opacity:0}, '-=1.4')
-	tl.from(["#t1", "#t2", "#t3", "#t4", "#t5"], .5, {opacity:0}, "+=.1")
-	tl.from(["#t6"], .5, {opacity:0})
+	tl.from(["#t1", "#t2", "#t3", "#t4", "#t5"], .9, {opacity:0, ease:Sine.easeOut}, "-=.4")
+	tl.from(["#t6"], .5, {opacity:0, ease:Sine.easeOut})
 
 	// return
 	
